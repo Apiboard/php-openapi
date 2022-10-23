4b41545f7e622717daee697c02be2130c3fe6042
Laurens Bultynck <laurens@3daimtrainer.com>
2022-10-23 21:04:51 +0200

    Add support for references on responses object

--- src/Structure/Responses.php
+++ src/Structure/Responses.php
@@ -3,26 +3,36 @@
 namespace Apiboard\OpenAPI\Structure;
 
 use Apiboard\OpenAPI\Concerns\AsCountableArray;
+use Apiboard\OpenAPI\Concerns\HasReferences;
+use Apiboard\OpenAPI\Contents\Reference;
 use ArrayAccess;
 use Countable;
 
 final class Responses implements ArrayAccess, Countable
 {
     use AsCountableArray;
+    use HasReferences;
 
     private array $data;
 
+    private array $responses;
+
     public function __construct(array $data)
     {
+        $this->data = $data;
+
         foreach ($data as $statusCode=>$value) {
-            $data[$statusCode] = new Response($statusCode, $value);
+            $data[$statusCode] = match ($this->isReference($value)) {
+                true => new Reference($value['$ref']),
+                default => new Response($statusCode, $value),
+            };
         }
 
-        $this->data = $data;
+        $this->responses = $data;
     }
 
-    public function offsetGet(mixed $statusCode): ?Response
+    public function offsetGet(mixed $statusCode): Response|Reference|null
     {
-        return $this->data[$statusCode] ?? null;
+        return $this->responses[$statusCode] ?? null;
     }
 }
--- tests/Structure/ResponsesTest.php
+++ tests/Structure/ResponsesTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Apiboard\OpenAPI\Contents\Reference;
 use Apiboard\OpenAPI\Structure\Response;
 use Apiboard\OpenAPI\Structure\Responses;
 
@@ -11,4 +12,28 @@ test('it can retrieve responses by their status code', function () {
     $result = $responses['200'];
 
     expect($result)->toBeInstanceOf(Response::class);
+});
+
+test('it can retrieve referenced responses by their status code', function () {
+    $responses = new Responses([
+        '200' => [
+            '$ref' => '#/some/ref',
+        ],
+    ]);
+
+    $result = $responses['200'];
+
+    expect($result)->toBeInstanceOf(Reference::class);
+});
+
+test('it can return all its references', function () {
+    $responses = new Responses([
+        "200" => [],
+        "404" => ['$ref' => '#/some/ref']
+    ]);
+
+    $result = $responses->references();
+
+    expect($result)->toHaveCount(1);
+    expect($result[0])->toBeInstanceOf(Reference::class);
 });
