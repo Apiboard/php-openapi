65219f6f7fe5d99e282292ee6bc399f4e62cf026
Laurens Bultynck <laurens@3daimtrainer.com>
2022-10-23 21:59:07 +0200

    Add references support to webhooks object

--- src/Structure/Webhooks.php
+++ src/Structure/Webhooks.php
@@ -3,26 +3,36 @@
 namespace Apiboard\OpenAPI\Structure;
 
 use Apiboard\OpenAPI\Concerns\AsCountableArray;
+use Apiboard\OpenAPI\Concerns\HasReferences;
+use Apiboard\OpenAPI\Contents\Reference;
 use ArrayAccess;
 use Countable;
 
 final class Webhooks implements ArrayAccess, Countable
 {
     use AsCountableArray;
+    use HasReferences;
 
     private array $data;
 
+    private array $webhooks;
+
     public function __construct(array $data)
     {
+        $this->data = $data;
+
         foreach ($data as $uri=>$value) {
-            $data[$uri] = new PathItem($uri, $value);
+            $data[$uri] = match ($this->isReference($value)) {
+                true => new Reference($value['$ref']),
+                default => new PathItem($uri, $value),
+            };
         }
 
-        $this->data = $data;
+        $this->webhooks = $data;
     }
 
-    public function offsetGet(mixed $offset): ?PathItem
+    public function offsetGet(mixed $offset): PathItem|Reference|null
     {
-        return $this->data[$offset] ?? null;
+        return $this->webhooks[$offset] ?? null;
     }
 }
--- tests/Structure/WebhooksTest.php
+++ tests/Structure/WebhooksTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Apiboard\OpenAPI\Contents\Reference;
 use Apiboard\OpenAPI\Structure\PathItem;
 use Apiboard\OpenAPI\Structure\Webhooks;
 
@@ -11,6 +12,18 @@ test('it can retrieve webhooks by their uri', function () {
     $result = $webhooks['/my-path'];
 
     expect($result)->toBeInstanceOf(PathItem::class);
+});
+
+test('it can retrieve referenced webhooks by their uri', function () {
+    $webhooks = new Webhooks([
+        '/my-path' => [
+            '$ref' => '#/some/ref',
+        ],
+    ]);
+
+    $result = $webhooks['/my-path'];
+
+    expect($result)->toBeInstanceOf(Reference::class);
 });
 
 test('it returns null for unknown webhooks', function () {
@@ -30,4 +43,16 @@ test('webhooks can be counted', function () {
     $result = count($webhooks);
 
     expect($result)->toBe(2);
+});
+
+test('it can return all its references', function () {
+    $responses = new Webhooks([
+        '/my-path' => [],
+        '/my-other-path' => ['$ref' => '#/some/ref']
+    ]);
+
+    $result = $responses->references();
+
+    expect($result)->toHaveCount(1);
+    expect($result[0])->toBeInstanceOf(Reference::class);
 });
